474c61dbacab82c1b52398bc6d202262c4be0cd3
Jpsito <jlamas723@gmail.com>
2026-09-09 04:45:26 +0000

    arreglar camara, gesto atras, vista previa y firma estable

--- android/app/src/main/java/com/apuntesu/app/MainActivity.java
+++ android/app/src/main/java/com/apuntesu/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.apuntesu.app;
 
+import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+                    bridge.getWebView().goBack();
+                } else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
+        });
+    }
+}
